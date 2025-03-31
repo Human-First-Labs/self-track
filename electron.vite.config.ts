b2c59f3987b51aad72feaf9a9a2a3c4efa7bc20b
Maurovic Cachia <maurovic.cachia@gmail.com>
2025-03-31 12:00:15 +0200

    Script Consolidation

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,20 +3,20 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()],
-    build: {
-      rollupOptions: {
-        external: ['node-desktop-idle-v2'] // Mark the module as external
-      }
-    }
+    plugins: [externalizeDepsPlugin()]
+    // build: {
+    //   rollupOptions: {
+    //     external: ['node-desktop-idle-v2'] // Mark the module as external
+    //   }
+    // }
   },
   preload: {
-    plugins: [externalizeDepsPlugin()],
-    build: {
-      rollupOptions: {
-        external: ['node-desktop-idle-v2'] // Mark the module as external
-      }
-    }
+    plugins: [externalizeDepsPlugin()]
+    // build: {
+    //   rollupOptions: {
+    //     external: ['node-desktop-idle-v2'] // Mark the module as external
+    //   }
+    // }
   },
   renderer: {
     plugins: [svelte()]
