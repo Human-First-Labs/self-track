f797ee0814abe0382a4bdaa1536193bc7ab55436
Maurovic Cachia <maurovic.cachia@gmail.com>
2025-08-09 11:52:47 +0200

    Jest tests for underlying package

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   transform: {
     '^.+\\.(ts|tsx)$': [`ts-jest`, { tsconfig: 'tsconfig.web.json' }]
+  },
+  moduleNameMapper: {
+    desktopIdle:
+      '<rootDir>/node_modules/node-desktop-idle-v2/prebuilds/linux-x64/node.napi.node-v127.node'
   }
 }
